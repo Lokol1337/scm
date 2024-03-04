236ac1429e246e8f255e3b632e49c15ffa412871
Lokol1337 <58722409+Lokol1337@users.noreply.github.com>
2024-03-04 16:48:39 +0300

    Update lab1.scm

--- lab1/lab1.scm
+++ lab1/lab1.scm
@@ -1,9 +1,9 @@
 (define TrigTable
     (begin
-		(format #t "~4a ~4a ~4a ~4a\n" "deg" "sin" "cos" "tan")
+		(format #t "~4a ~4a ~4a ~4a\n" "rad" "sin" "cos" "tan")
 		(lambda (count)
 			(let loop ((count count))
-				 (if (= count 180) `quit
+				 (if (> count 180) `quit
 					 (begin
 						 (format #t "~4a ~4f ~4f ~4f\n" count (sin count) (cos count) (tan count))
 						 (loop (+ 5 count))
